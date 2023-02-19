18d3fcc22d9ad0b1ce95d6813853f327350bf76d
Denny <Dennyng1989@hotmail.com>
2023-02-19 15:38:32 -0800

    commit iterative version of spinner

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+let delay = 0;
+let repeats = 6;
+
+for (let i = 0; i <= repeats; i++) {
+  spinner.forEach(x => {
+    setTimeout(() => process.stdout.write(x), delay);
+    delay += 100;
+  });
+}
+
+setTimeout(() => console.log(), delay);
